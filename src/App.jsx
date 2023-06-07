0a4fed261243c63c58d306e9204841c6987aaaf9
PhoePA <pyaephyoaung.wcs@gmail.com>
2023-06-07 11:20:30 +0630

    Redux counter project with Redux Toolkit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div className="logo">
+      {" "}
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
+ 
